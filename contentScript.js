51baddbb1168c9f501b1022a768529fc8bb247f7
Jurgen Jansma <jurgen.jansma@bitfactory.nl>
2023-06-27 13:34:23 +0200

    Only click when button exists

--- contentScript.js
+++ contentScript.js
@@ -2,13 +2,21 @@ chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         // listen for messages sent from background.js
         if (request.message === 'link-changed' && window.location.href.includes('https://portal.bitfactory.nl/ticket/')) {
-            document.querySelector('[data-action="COMMENT"]').click();
+            const button = document.querySelector('[data-action="COMMENT"]');
+
+            if (button) {
+                button.click();
+            }
         }
     });
 
 window.addEventListener("load", () => {
     if (window.location.href.includes('https://portal.bitfactory.nl/ticket/')) {
-        document.querySelector('[data-action="COMMENT"]').click();
+        const button = document.querySelector('[data-action="COMMENT"]');
+
+        if (button) {
+            button.click();
+        }
     }
 }, false);
 
